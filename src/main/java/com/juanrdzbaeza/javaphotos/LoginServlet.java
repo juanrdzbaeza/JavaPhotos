05a2b07b2684ef861a799de2d6ccb3880eb7fb69
Juan <juan.rodriguez@edosoft.es>
2025-04-29 10:01:35 +0100

    Implements user login functionality
    Adds a login form to index.jsp and creates a LoginServlet to handle user authentication.
    The servlet validates user credentials against a UserDAO and redirects to home.jsp upon successful login.

---
+++ src/main/java/com/juanrdzbaeza/javaphotos/LoginServlet.java
@@ -0,0 +1,44 @@
+package com.juanrdzbaeza.javaphotos;
+
+import java.io.*;
+
+import com.juanrdzbaeza.javaphotos.dao.UserDAO;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "loginServlet", value = "/login-servlet")
+public class LoginServlet extends HttpServlet {
+
+
+    private final JavaPhotosLogic javaPhotosLogic = new JavaPhotosLogic(
+            new com.juanrdzbaeza.javaphotos.util.DatabaseConnection() // Inicializa la conexión a la base de datos
+    );
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        response.setContentType("text/html");
+
+        // Obtener los datos del formulario
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        // Validar los datos con UserDAO
+        UserDAO userDAO = new UserDAO();
+        boolean isValidUser = userDAO.validateUser(username, password);
+
+        // Generar la respuesta
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        if (isValidUser) {
+            response.sendRedirect("home.jsp");
+        } else {
+            out.println("<h1>Login failed! Invalid username or password.</h1>");
+            out.println("<a href=\"index.jsp\">Back to the login</a>");
+        }
+        out.println("</body></html>");
+
+
+    }
+
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -5,9 +5,16 @@
     <title>JSP - Hello World</title>
 </head>
 <body>
-<h1><%= "Hello World!" %>
-</h1>
-<br/>
-<a href="hello-servlet">Hello Servlet</a>
+<h1>Login</h1>
+<form action="login-servlet" method="post">
+    <label for="username">Username:</label>
+    <input type="text" id="username" name="username" required>
+    <br>
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password" required>
+    <br>
+    <button type="submit">Login</button>
+</form>
+<button onclick="location.href='signup.jsp'">Sign Up</button>
 </body>
 </html>
